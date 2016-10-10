0e5bc714b111dbe10816ef06e857c5dbbad66fbd
DESKTOP-FT8898I\lasse <lasse_d_c@hotmail.com>
2016-10-10 13:56:40 +0200

    database schemas,some dataset files, and matlab command examples.

---
+++ Recommender/Small command resume.m
@@ -0,0 +1,13 @@
+%Import commands for files
+oneKUsers = importdata('../dataset/lastfm-dataset-1K/userid-profile.tsv');
+b = importdata('../dataset/test.txt');
+%disp command for printing the variables
+%disp(oneKUsers);
+%disp(b);
+%Use the sparse() command to make a sparse representation of a int matrix
+sparseB = sparse(b);
+%disp(sparseB);
+%How to use Nonnegative-MF on a int matrix.
+[X,Y] = nnmf(sparseB, 2);
+%disp(X);
+%disp(Y);
